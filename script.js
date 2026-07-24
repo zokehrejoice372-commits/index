6cd894d4dd2a5e4b5a80a2ef3bd3eccde638e01c
zokehrejoice372-commits <zokehrejoice372@gmail.com>
2026-07-23 23:20:38 -1200

    adding java

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleInfo() {
+  const info = document.getElementById("moreInfo");
+
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
+}
